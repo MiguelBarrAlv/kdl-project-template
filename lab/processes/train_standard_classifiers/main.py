7b7fafeab9501e54507ad8e042b7b4fd4b9760a8
Miguel Barrios <miguel.barrios@intelygenz.com>
2023-09-18 14:03:08 +0200

    feat: Created object for deploying cluster in Azure ML Studio through SDK

--- lab/processes/train_standard_classifiers/main.py
+++ lab/processes/train_standard_classifiers/main.py
@@ -8,6 +8,7 @@ import os
 import mlflow
 
 from lab.processes.train_standard_classifiers.classifiers import train_classifiers
+from lab.processes.azure.cluster import ComputeManager
 from lab.processes.azure.train import MLFlowManager
 from lab.processes.azure.storage import AzureDatastoreManager
 
@@ -20,13 +21,14 @@ MLFLOW_TAGS = {"git_tag": "mlflow-azure"} # NOTE: Added git_tag to MLFLOW_TAGS h
 
 if __name__ == "__main__":
 
-    manager_mlflow = MLFlowManager(experiment_name=config["mlflow"]["mlflow_experiment"])
-    manager_blob = AzureDatastoreManager()
+    ComputeManager()
+    # manager_mlflow = MLFlowManager(experiment_name=config["mlflow"]["mlflow_experiment"])
+    # manager_blob = AzureDatastoreManager()
     
-    asyncio.run(train_classifiers(
-        manager_mlflow=manager_mlflow,
-        manager_blob=manager_blob,
-        config=config,
-        mlflow_tags=MLFLOW_TAGS
-    ))
+    # asyncio.run(train_classifiers(
+    #     manager_mlflow=manager_mlflow,
+    #     manager_blob=manager_blob,
+    #     config=config,
+    #     mlflow_tags=MLFLOW_TAGS
+    # ))
 
